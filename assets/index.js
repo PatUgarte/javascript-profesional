46ef426835beccb3ba065fad1ad32919f4b12375
Pat Ugarte <patriciomugarte@gmail.com>
2020-04-30 19:48:05 -0300

    Agregado el script al HTML para añadir funcionalidad al botón de play/pause

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+
+function MediaPlayer(config) {
+    this.media = config.element;
+}
+
+MediaPlayer.prototype.play = function () {
+    this.media.play();
+}
+
+MediaPlayer.prototype.pause = function () {
+    this.media.pause();
+}
+
+MediaPlayer.prototype.togglePlayPause = function () {
+    this.media.paused
+        ? this.play()
+        : this.pause();
+}
+
+const player = new MediaPlayer({ element: video });
+button.onclick = () => player.togglePlayPause();
