c72c1ff65766492d17e134d658d06f3d12ea1692
Pat Ugarte <patriciomugarte@gmail.com>
2020-05-02 13:48:54 -0300

    Arreglos menores

--- assets/index.ts
+++ assets/index.ts
@@ -12,7 +12,10 @@ playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
 
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("../sw.js").catch(error => {
-        console.log(error.message);
-    });
+    navigator
+        .serviceWorker
+        .register("../sw.js")
+        .catch(error => {
+            console.log(error.message);
+        });
 }
